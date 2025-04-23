edd84e07c71d0587c65ae2f35660cb83b86069d7
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-04-23 18:30:06 +0200

    collegamneto funzionante

--- js/main.js
+++ js/main.js
@@ -2,34 +2,36 @@ const total = 10;
 const emailUrl = `https://flynn.boolean.careers/exercises/api/random/mail`
 const emailArray = [];
 let totalEmail;
+const containerCard = document.querySelector(".container-card");
+let cardHTML = ``;
 
 axios.get(emailUrl)
     .then(response => {
         const emailRandom = response.data.response;        
-        //console.table(emailRandom); 
+        console.table(emailRandom); 
         
         for (let i = 0; i < 10; i++) {
             totalEmail = emailArray.push([i] + emailRandom);
             console.log(emailArray);
           }
+          for (let i = 0; i < 10; i++){
+            cardHTML +=`
+                    <div class="card-list">
+                        <ul class="list">
+                            <div class="li">${emailArray[i]}</div>
+                        </ul>
+                    </div>
+            `
+    }
+    containerCard.innerHTML = cardHTML;
+    
     });
 
 
 
 
-const containerCard = document.querySelector(".container-card");
-let cardHTML = ``;
-    
-for (let i = 0; i < 10; i++){
-        cardHTML +=`
-                <div class="card-list">
-                    <ul class="list">
-                        <div class="li">${emailArray.emailRandom}</div>
-                    </ul>
-                </div>
-        `
-}
-containerCard.innerHTML = cardHTML;
+
+
 
 
 
