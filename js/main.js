15c2b968b97b048535a6007b4669b1a5632333c7
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-04-23 16:54:46 +0200

    aggiunto array

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
-let data;
+const total = 10;
+const emailUrl = `https://flynn.boolean.careers/exercises/api/random/mail`
+const emailArray = [];
+let totalEmail;
 
-axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-.then(response => {
-    data = response;
-console.log(data);
-});
+axios.get(emailUrl)
+    .then(response => {
+        const emailRandom = response.data.response;        
+        console.table(emailRandom); 
+        for (let i = 0; i < 10; i++) {
+            totalEmail = emailArray.push([i] + emailRandom);
+            console.log(emailArray);
+          }
+    });
+
+
+
+
+   
