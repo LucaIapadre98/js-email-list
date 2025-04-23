6625e266f9469fc549bc371686c3a5d4bdb0aeaa
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-04-23 17:14:58 +0200

    aggiunta la lista all'HTML

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@ let totalEmail;
 axios.get(emailUrl)
     .then(response => {
         const emailRandom = response.data.response;        
-        console.table(emailRandom); 
+        //console.table(emailRandom); 
+        
         for (let i = 0; i < 10; i++) {
             totalEmail = emailArray.push([i] + emailRandom);
             console.log(emailArray);
